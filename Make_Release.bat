f0a4fb04025d300707696bd365b2692900c7fbe2
DevonArtmeier <devon.artmeier@outlook.com>
2024-04-19 22:11:12 -0400

    Audio test

--- Make_Release.bat
+++ Make_Release.bat
@@ -1,3 +1,5 @@
 @echo off
 cmake --build build --config Release
+copy build\violet-engine\dependencies\SDL\Release\SDL3.dll build\Release\SDL3.dll
+copy data\*.* build\Release\*.*
 pause
---
+++ source/main.cpp
@@ -0,0 +1,20 @@
+#include <violet_engine.hpp>
+
+namespace Violet
+{
+    void GameInit()
+    {
+        OpenSound("test", "test.ogg");
+        LoopSound("test", 1);
+    }
+
+    void GameUpdate()
+    {
+
+    }
+
+    void GameClose()
+    {
+
+    }
+}
