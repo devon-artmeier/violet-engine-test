64cc48a0d6b7d6558b9af05c96cbc175956e1911
DevonArtmeier <devon.artmeier@outlook.com>
2024-04-21 03:09:21 -0400

    Only play 1 sound

--- Make_Debug.bat
+++ Make_Debug.bat
@@ -1,5 +1,5 @@
 @echo off
 cmake --build build --config Debug
-copy build\violet-engine\dependencies\SDL\Debug\SDL3.dll build\Release\SDL3.dll
+copy build\violet-engine\dependencies\SDL\Debug\SDL3.dll build\Debug\SDL3.dll
 copy data\*.* build\Debug\*.*
 pause
--- source/main.cpp
+++ source/main.cpp
@@ -4,15 +4,8 @@ void GameInit()
 {
     Violet::SetMasterVolume(50);
 
-    Violet::LoadSound("test1", "yeah.wav");
-    Violet::LoadSound("test2", "honk.mp3");
-    Violet::LoadSound("test3", "test.ogg");
-    Violet::LoadSound("test4", "whistle.flac");
-    
-    Violet::PlaySound("test1", 4);
-    Violet::PlaySound("test2", 4);
-    Violet::PlaySound("test3", 4);
-    Violet::PlaySound("test4", 4);
+    Violet::LoadSound("yeah", "yeah.wav");
+    Violet::PlaySound("yeah", 0);
 }
 
 void GameUpdate()
