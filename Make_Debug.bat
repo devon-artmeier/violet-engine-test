edae849daa9b43e14aec000511677dd58f36f214
DevonArtmeier <devon.artmeier@outlook.com>
2024-04-24 18:38:46 -0400

    Update violet-engine

--- Make_Debug.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cmake --build build --config Debug
-copy build\violet-engine\dependencies\SDL\Debug\SDL3.dll build\Debug\SDL3.dll
-copy data\*.* build\Debug\*.*
-pause
--- Make_Release.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cmake --build build --config Release
-copy build\violet-engine\dependencies\SDL\Release\SDL3.dll build\Release\SDL3.dll
-copy data\*.* build\Release\*.*
-pause
