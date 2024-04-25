92ebdd5ffac8ce7f7a9b9247ae6a2c78ae89ac6d
DevonArtmeier <devon.artmeier@outlook.com>
2024-04-24 20:59:03 -0400

    Draw sprite

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,9 @@
 
 void GameInit()
 {
+    Violet::LoadTexture("texture", "data/texture.png");
+    Violet::LoadSpriteSheet("sprites", "data/sprites.spr", "texture");
+
     Violet::SetMasterVolume(50);
 
     Violet::LoadSound("yeah", "data/yeah.wav");
@@ -10,7 +13,11 @@ void GameInit()
 
 void GameUpdate()
 {
-
+    static float angle = 0.0;
+    static float scale = 0.0f;
+    Violet::DrawSprite("sprites", 0,
+                       { 0, Violet::GetWindowWidthPixels() / 2.0f, Violet::GetWindowHeightPixels() / 2.0f, scale, scale, angle++});
+    scale += 0.005f;
 }
 
 void GameClose()
