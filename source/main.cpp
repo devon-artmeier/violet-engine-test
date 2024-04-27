cc31a5fcb47ab18d8051161c72dfcd0264634e7b
DevonArtmeier <devon.artmeier@outlook.com>
2024-04-27 11:33:41 -0400

    Update violet-engine

--- source/main.cpp
+++ source/main.cpp
@@ -13,9 +13,12 @@ void GameInit()
 
 void GameUpdate()
 {
+    static float x     = Violet::GetWindowWidthPixels() / 2.0f;
+    static float y     = Violet::GetWindowHeightPixels() / 2.0f;
     static float angle = 0.0f;
     static float scale = 0.0f;
-    Violet::DrawSprite("sprites", 0, 0, { Violet::GetWindowWidthPixels() / 2.0f, Violet::GetWindowHeightPixels() / 2.0f }, { scale, scale }, angle++);
+
+    Violet::DrawSprite("sprites", 0, 0, x, y, scale, scale, angle++);
     scale += 0.005f;
 }
 
