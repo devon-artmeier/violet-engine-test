4f4508539a76db358d357d943a3ef2a78aeedec1
DevonArtmeier <devon.artmeier@outlook.com>
2024-04-27 20:31:26 -0400

    Update demo

--- source/main.cpp
+++ source/main.cpp
@@ -6,23 +6,26 @@ void GameInit()
 
     Violet::LoadTexture    ("texture", "data/texture.png");
     Violet::LoadSpriteSheet("sprites", "data/sprites.spr", "texture");
-    Violet::LoadSound      ("yeah",    "data/yeah.wav");
-    Violet::LoadSound      ("cirno",   "data/cirno.ogg");
+    Violet::LoadSound      ("music",   "data/music.ogg");
 
-    Violet::SetSoundLoop   ("cirno", 255387, 3949028);
-    Violet::LoopSound      ("cirno");    
-    Violet::PlaySound      ("yeah");
+    Violet::LoopSound      ("music");
 }
 
 void GameUpdate()
 {
     static float x     = Violet::GetWindowWidthPixels() / 2.0f;
     static float y     = Violet::GetWindowHeightPixels() / 2.0f;
+    static float x_off = Violet::GetWindowWidthPixels() / 4.0f;
     static float angle = 0.0f;
-    static float scale = 0.0f;
+    
+    float sine_x  = Violet::Math::SinDeg(angle);
+    float scale_x = ((Violet::Math::SinDeg(angle * 4) + 1.0f) / 4.0f) + 0.75f;
+    float scale_y = ((Violet::Math::CosDeg(angle * 4) + 1.0f) / 4.0f) + 0.75f;
+    float pitch   = ((Violet::Math::SinDeg(angle) + 1.0f) / 7.0f) + 0.85f;
 
-    Violet::DrawSprite("sprites", 0, 0, x, y, scale, scale, angle++);
-    scale += 0.005f;
+    Violet::DrawSprite     ("sprites", 0, 0, x + (sine_x * x_off), y, scale_x, scale_y, angle++);
+    Violet::SetSoundPanning("music", sine_x);
+    Violet::SetSoundPitch  ("music", pitch);
 }
 
 void GameClose()
