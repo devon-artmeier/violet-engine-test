6f7c2c51696b966a0dae1a24b3a3f8cfdfa5ac6e
DevonArtmeier <devon.artmeier@outlook.com>
2024-05-05 16:31:35 -0400

    Update violet-engine

--- source/main.cpp
+++ source/main.cpp
@@ -19,17 +19,17 @@ void GameUpdate()
     static float x_off = Violet::GetWindowWidthPixels() / 4.0f;
     static float angle = 0.0f;
     
-    float sine_x  = Violet::Math::SinDeg(angle);
-    float scale_x = ((Violet::Math::SinDeg(angle * 4) + 1.0f) / 4.0f) + 0.75f;
-    float scale_y = ((Violet::Math::CosDeg(angle * 4) + 1.0f) / 4.0f) + 0.75f;
-    float pitch   = ((Violet::Math::SinDeg(angle) + 1.0f) / 7.0f) + 0.85f;
+    float sine_x  = Violet::Math::SinDegrees(angle);
+    float scale_x = ((Violet::Math::SinDegrees(angle * 4) + 1.0f) / 4.0f) + 0.75f;
+    float scale_y = ((Violet::Math::CosDegrees(angle * 4) + 1.0f) / 4.0f) + 0.75f;
+    float pitch   = ((Violet::Math::SinDegrees(angle) + 1.0f) / 7.0f) + 0.85f;
 
-    Violet::DrawSprite     ("sprites", 0, 0, x + (sine_x * x_off), y, scale_x, scale_y, angle++);
+    Violet::DrawSprite     ("sprites", 0, 0, Violet::Vector2D(x + (sine_x * x_off), y), angle++, Violet::Vector2D(scale_x, scale_y));
     Violet::SetSoundPanning("music", sine_x);
     Violet::SetSoundPitch  ("music", pitch);
 
-    Violet::DrawText("font", 64, "Hello World!", 0, 34, 34, { 0, 0, 0, 255 });
-    Violet::DrawText("font", 64, "Hello World!", 0, 32, 32, { 255, 255, 255, 255 });
+    Violet::DrawText("font", 64, "Hello World!", 0, Violet::Vector2D(34, 34), { 0, 0, 0, 255 });
+    Violet::DrawText("font", 64, "Hello World!", 0, Violet::Vector2D(32, 32), { 255, 255, 255, 255 });
 }
 
 void GameClose()
